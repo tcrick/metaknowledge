72d7af45a99a50bb82f6ecb4d1e7850b8db72279
Tyler Crick <crick.tyler@gmail.com>
2018-11-14 21:45:44 -0500

    modified conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ release = '3.3.1'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.8.1'
+needs_sphinx = '1.7.9'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
